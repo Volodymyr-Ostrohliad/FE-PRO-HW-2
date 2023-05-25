3766f9d2da76755583c62d30cae0e993d0257711
Volodymyr-Ostrohliad <willhonorodua@gmail.com>
2023-05-25 18:20:29 +0300

    Add HW

---
+++ index.js
@@ -0,0 +1,12 @@
+const name = prompt('What is your name?');
+let message;
+
+if (name === null || name === '' || !isNaN(name)) {
+  message = "Sorry, you didn't want to enter your name.";
+} else {
+  message = `Hello, ${name}! How are you?`;
+}
+
+alert(message);
+console.log(message);
+console.log(name);
