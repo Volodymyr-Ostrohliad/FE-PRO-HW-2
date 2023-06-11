0f1f0747d27e1546925faa834e1a762d8e06d813
Volodymyr Ostrohliad <willhonorodua@gmail.com>
2023-06-11 20:53:46 +0300

    .

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const name = prompt('What is your name?');
 let message;
 
 if (!name || !isNaN(name)) {
-  message = "Sorry, you didn't want to enter your name.";
+  message = "Sorry, you didn't want to enter your name!";
 } else {
   message = `Hello, ${name}! How are you?`;
 }
